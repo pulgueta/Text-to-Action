5c0a8247143e6aa9f6a5ed4a28ae7e59368cfda4
pulgueta <retardix456@gmail.com>
2023-12-19 06:03:38 -0500

    Updated jest config to not throw errors in production

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,19 @@
 import nextJest from 'next/jest';
 
+const ignoredErrors = [
+	/act\(\.\.\.\) is not supported in production builds of React/,
+];
+
+const consoleError = global.console.error;
+
+global.console.error = (...args) => {
+	if (ignoredErrors.some((el) => el.test(args[0]))) {
+		return console.log(...args);
+	}
+
+	return consoleError(...args);
+};
+
 const createConfig = nextJest({
 	dir: './',
 });
